309d9f8e95db5021b1bb16747e2266565b5def42
barkinspmz <barkinsapmaz2@outlook.com>
2023-10-23 22:43:49 +0300

    General informations about C#.

--- CSharpBasicsRecap/Assets/CSharpSyntaxAndBasicsScripts/GeneralInfoAboutCSharp.cs
+++ CSharpBasicsRecap/Assets/CSharpSyntaxAndBasicsScripts/GeneralInfoAboutCSharp.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GeneralInfoAboutCSharp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /*
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+             C# is a programming language which is developed by the Microsoft that runs on the .NET Framework.
+             We can develop web apps, mobile apps, games and etc with using C#.
+             It is an OOP language. It has roots the C family. The first version released in 2002.
+
+           C# Syntax in Unity; 
+           
+           The method named Start() works when the game has started. You can declare your variables or doing your actions
+           in there once when started the game. It will work for one time only.
+     
+           The method named Update() works once per frame. Declaring variable in that method is not a good way. You can 
+           check for some events in this method such as the player's input.
+
+           Every C# statement ends with a semicolon. Ex: int a = 10; - You can think this as dot at the end of sentence.
+
+    */
 }
